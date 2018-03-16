aa25c325f64d4da04b830e6b982aac4edbc3bca0
Kevin Perkins <kevin.p@ITs-MacBook-Pro-3.local>
2018-03-16 15:09:29 -0400

    initialize ARkit project

---
+++ TargetPractice/Scene.swift
@@ -0,0 +1,25 @@
+//
+//  Scene.swift
+//  TargetPractice
+//
+//  Created by Kevin Perkins on 3/16/18.
+//  Copyright © 2018 Kevin Perkins. All rights reserved.
+//
+
+import SpriteKit
+import ARKit
+
+class Scene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // Setup your scene here
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+}
